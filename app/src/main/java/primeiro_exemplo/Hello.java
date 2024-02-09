f8e5e023eb337151dfc59bf108909cc3f9cae908
ana-aha laura <aninha.20.07.oli@gmail.com>
2024-02-09 15:09:08 +0000

    variaveis

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -2,6 +2,10 @@ package primeiro_exemplo;
 
 public class Hello {
     public static void main(String[] args) {
+        int cont = 16;
+        String nome = "Ana";
         System.out.println("Olá Mundo!");
+
+        System.out.println(cont + " : " + nome);
     }
 }
